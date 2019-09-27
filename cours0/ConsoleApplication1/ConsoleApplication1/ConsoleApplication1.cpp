c26dd117b4e7d9aff9bba4cba96d046cbcce472f
David Elahee <david.elahee@gmail.com>
2019-09-27 16:19:03 +0200

    add 64 bit

--- cours0/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ cours0/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,11 +3,132 @@
 
 #include "pch.h"
 #include <iostream>
+#include <cstdlib>//#include "stdlib.h"
+#include <cstdio>//#include "stdio.h"
+#include <chrono>
+
+int appel( int & s ) {
+	s++;
+	return s;
+}
+
+struct Vec3 {
+	int  x;
+	int y;
+	int z;
+};
+
+Vec3 incrX(Vec3 _in){
+	_in.x++;
+	return _in;
+}
+
+Vec3 stackOverflow(Vec3 _in) {
+	Vec3 temp = _in;
+	temp.y++;
+	return stackOverflow(temp);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	//Vec3 toto;
+	/*int x = 66;
+	appel(x);
+	printf("xval: %d\n", x);*/
+	//68 ?
+	//Vec3 toto = { 1,2,3 };//new Vec3()
+	//incrX(toto);
+	//printf("xval: %f\n", toto.x);
+
+	//int  * v = (int*)malloc(4);
+
+	//Vec3 bob = { 1,2,3 };
+	//bob = stackOverflow( bob );
+	//printf(" val x : %f\n", bob.x);
+
+	Vec3 vecTab[3];
+	vecTab[0] = { 66,66,66 };
+	vecTab[1] = { 4,5,6 };
+	vecTab[2] = { 7,8,9 };
+	printf("v0x %d\n", vecTab[0].x);
+	vecTab[0].x++;
+	//printf("v0x %f\n", vecTab[0]);
+
+	Vec3 * t0 = 0;
+	Vec3 * t1 = nullptr;
+	Vec3 * t2 = &vecTab[1];
+
+	//*(t2.y) = 555;
+	(*t2).y = 777;
+	t2->y = 888;
+	
+	Vec3* iter = &vecTab[0];
+	int  i = 0;
+	for (i = 0; i < 3; ++i) {
+		///printf("val vec x: %d\n", iter->x);
+		iter++; 
+		//iter = iter + 1;
+		//iter = ( &vecTab[0] + i );
+		//iter[i];
+	}
+
+	Vec3* tab = &vecTab[0];
+	for (i = 0; i < 3; ++i) {
+		printf("val vec x: %d\n", tab[i]);
+	}
+
+
+	Vec3 * t3 = t2+1;//?? T ptr + 1 => *ptr += sizeof(T)
+	t2++;//
+
+	const char label2[6] = { 's', 'a','p','i','n', 0 };
+
+	/*
+	const char * ptr = &label2[0];
+	ptr++;
+	printf("%c\n", *ptr);
+	*/
+
+	//int _i = 0;
+
+	//int * ptr = (int*)0x3fdd00ee;
+	//(*ptr)++;
+
+	auto start = std::chrono::system_clock::now();
+	int * bigBlock = (int*) malloc(1024 * 1024 * 1024);
+	for (int k = 0; k < 64 * 1024 * 1024;++k) {
+		bigBlock[k] = 0xdeadbeef;
+	}
+	printf("beef ? : %x\n", bigBlock[1024 * 1024]);
+	auto end = std::chrono::system_clock::now();
+	auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	printf("time ? : %d\n", millis);
+	int _i = 0;
 }
+
+
+//Debug.Log(monobjet);
+/*
+std::cout << "Hello World!\n" << 66 << "\n";
+
+int foo = 0;
+
+const char * label = "sapin";
+const char label2[6] = { 's', 'a','p','i','n', 0 };
+
+float v = 0.5f;
+double ff = 0.5;
+*/
+//printf("Hello world this is a test : %d label : %s %f\n", foo,label,66.66f);
+//printf("Hello world this is a test : a fraction %f \n", ((float)1.0f/3.0f));
+//printf("Hello world this is a test : a fraction %f \n", ((double)1.00 / 3.0));
+//printf("Hello world : l2: %s\n", label2);
+
+//const char labelBug[5] = { 's', 'a','p','i','n' };
+//printf("Hello world : lb: %s\n", labelBug);
+//std::string labelcpp = "vachement sympa";
+
+//printf("%s\n", labelcpp.c_str());
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
 // Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
--- cours0/ConsoleApplication1/ConsoleApplication1/MonHeader.hpp
+++ cours0/ConsoleApplication1/ConsoleApplication1/MonHeader.hpp
@@ -1,8 +1,36 @@
 #pragma once
 
 
-struct Vec3{
+
+
+/*
+typedef struct Vec3 {
 	float x;
 	float y;
 	float z;
+} VEC3;
+*/
+/*
+struct LIST;
+typedef struct List {
+	LIST p;
+} LIST;
+*/
+struct MonEpee; //{}
+
+struct Mabotte {
+	MonEpee * x;
+};
+
+struct MonEpee {
+	Mabotte * x;
+};
+
+typedef int MONINT;
+typedef int IDENTIFIANT;
+
+class Player {
+public:
+	IDENTIFIANT keyId;
+	IDENTIFIANT Passeportid;
 };
