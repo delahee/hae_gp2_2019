08d1826ce83872efeadb7fdb00c9665f50e1fcf0
David Elahee <david.elahee@gmail.com>
2019-10-11 14:45:26 +0200

    we made a mistake

--- cours2/Cours2/IntArray.cpp
+++ cours2/Cours2/IntArray.cpp
@@ -41,10 +41,19 @@ void IntArray::push_front(int elem) {
 }
 
 void IntArray::insert(int pos, int elem) {
-	ensure( ( pos>=curSize ) ? (curSize + 1) : (pos+1) );
+	/**
+	if( ( pos>=curSize ) ){
+		ensure(pos+1);
+	else
+		ensure( curSize +1 );
+	*/
+	ensure( ( pos>=curSize ) ? (pos + 1) : (curSize+1) );
+
 	for (int i = curSize; i > pos; i--) 
 		data[i] = data[i - 1];
+
 	data[pos] = elem;
+
 	if( pos >= curSize )
 		curSize = pos + 1;
 	else 
