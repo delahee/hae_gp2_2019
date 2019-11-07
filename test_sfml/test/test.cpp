22257513287bb5e145ed750f8703577fe3056c66
Delahee <Delahee@users.noreply.github.com>
2019-11-07 18:01:07 +0100

    ...

--- test_sfml/test/test.cpp
+++ test_sfml/test/test.cpp
@@ -2,29 +2,73 @@
 //
 
 #include "pch.h"
+
+#include "Lib.h"
 #include <iostream>
 
 #include <SFML/Graphics.hpp>
+
+using namespace sf;
 
 int main()
 {
     std::cout << "Hello World!\n"; 
 
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+	//sf::CircleShape shape(100.f);
+	//shape.setFillColor(sf::Color::Green);
 
+	sf::Clock	clock;
+
+	sf::Time	appStart = clock.getElapsedTime();
 	while (window.isOpen())
 	{
+		sf::Time now = clock.getElapsedTime();
+		sf::Time dt = appStart - now;
+
+		if (dt.asSeconds() < 0.001f ) dt = sf::seconds(0.001f);
+
 		sf::Event event;
+
+		sf::VertexArray line(sf::LinesStrip);
+		line.append(Vertex(Vector2f(10,10), sf::Color(255,0,0)));
+		line.append(Vertex(Vector2f(100,100)));
+		line.append(Vertex(Vector2f(100,50)));
+
+
+		sf::VertexArray curve(sf::LinesStrip);
+
+		std::vector<float> lx;
+		lx.push_back(50);
+		lx.push_back(75);
+		lx.push_back(120);
+		lx.push_back(180);
+
+		std::vector<float> ly;
+		ly.push_back(60);
+		ly.push_back(85);
+		ly.push_back(100);
+		ly.push_back(280);
+
+		Color red(240, 80, 40);
+
+		for (int i = 0; i < 50; i++) {
+			curve.append(Vertex(
+				Vector2f(	Lib::plot(i / 50.0f, lx),
+							Lib::plot(i / 50.0f, ly )),
+				red));
+		}
+		
 		while (window.pollEvent(event))
 		{
-			//if (event.type == sf::Event::Closed)
-			//	window.close();
+			if (event.type == sf::Event::Closed)
+				window.close();
 		}
 
 		window.clear();
-		window.draw(shape);
+		window.draw(line);
+
+		window.draw(curve);
 		window.display();
 	}
 
