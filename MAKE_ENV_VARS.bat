d7ce3efe431e52545cf843b11d48c469777cd5fd
David Elahee <david.elahee@gmail.com>
2019-11-22 10:33:47 +0100

    various lib dir

--- MAKE_ENV_VARS.bat
+++ MAKE_ENV_VARS.bat
@@ -1,2 +1,5 @@
 SETX BOX2D_DIR "C:\Workspace\delahee\Box2D"
+SETX SFML_DIR "C:\Workspace\delahee\hae_gp2\sfml"
+SETX IMGUI_DIR "C:\Workspace\delahee\imgui"
+SETX SFML_IMGUI_DIR "C:\Workspace\delahee\imgui-sfml"
 PAUSE
