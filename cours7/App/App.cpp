7b937ffa9b0299f30af464a9ece3ea0371e244d7
David Elahee <david.elahee@gmail.com>
2019-11-29 17:14:34 +0100

    add parser

--- cours7/App/App.cpp
+++ cours7/App/App.cpp
@@ -1,6 +1,7 @@
 // App.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
 
+
 #include "pch.h"
 
 #include <iostream>
@@ -45,6 +46,8 @@ static sf::Texture * whiteTex = nullptr;
 static sf::Transform				s_Initial;
 static std::vector<sf::Transform>	s_Trs;
 static std::vector<Turtle*>			s_Turtles;
+
+
 
 
 static void startTransforms() {
@@ -154,6 +157,80 @@ static void koch_n(int n, float len) {
 	//shift_up(-200);
 }
 
+bool startsWith(const char * s0, const char * s1) {
+	if (*s0 == 0 && *s1 != 0)
+		return false;
+	if (*s1 == 0)
+		return true;
+	if (*s0 != *s1)
+		return false;
+	else
+		return startsWith(s0 + 1, s1 + 1);
+}
+
+static char data[1024];
+enum TurtleCommand{
+	AV,
+	//add other commands
+};
+static std::vector<TurtleCommand> cmd;
+static void readScript() {
+	cmd.clear();
+
+	FILE * f = nullptr;
+	auto err = fopen_s(&f,"res/scriptTortue.txt","r");
+	if (f != nullptr) {
+		memset(data, 0, 1024);
+		fread(data, 2014, 1, f);
+		printf("read file %s\n", data);
+		fclose(f);
+
+		char * cur = data;
+		bool doContinue = true;
+
+		while (doContinue) {
+			if (cur == nullptr || *cur == 0) break;
+			else {
+				if (startsWith(cur, "AV")) {//is command AV
+					cmd.push_back(AV);//add command to list
+				}
+				
+				//your other commands here
+
+				cur = strstr(cur, " ");//reach next space character
+				if (!cur)
+					break;
+				else
+					cur++;
+			}
+		}
+	}
+	else {
+		printf("cannot read file %s %d\n", "res/scriptTortue.txt", err);
+	}
+}
+
+
+class Repeater {
+public:
+	float duration;
+	float current;
+	std::function<void(void)> cbk;
+
+	Repeater(float duration, std::function<void(void)> action) {
+		cbk = action;
+		this->duration = duration;
+		current = duration;
+	}
+
+	void update(float dt) {
+		current -= dt;
+		if (current <= 0) {
+			if (cbk)cbk();
+			current = duration;
+		}
+	}
+};
 
 int main() {
 
@@ -194,10 +271,7 @@ int main() {
 	unsigned int col = 0xffffffff;
 	whiteTex->update((const sf::Uint8*)&col,1,1,0,0);
 
-	gradientMountain = new Texture();
-	if (!gradientMountain->loadFromFile("res/mountain.png")) {
-		printf("unable to load tex\n");
-	}
+	readScript();
 
 	sf::Text myFpsCounter;
 	int every = 0;
@@ -211,6 +285,10 @@ int main() {
 
 	startTransforms();
 	plotTurtle();
+
+	Repeater rp(0.1,[]() {
+		printf("reapeating\n");
+	});
 
 	while (window.isOpen())//on passe tout le temps DEBUT DE LA FRAME 
 	{
@@ -264,6 +342,23 @@ int main() {
 
 					if (event.key.code == sf::Keyboard::F1) { koch_n(4, 300);			plotTurtle(); }
 
+					if (event.key.code == sf::Keyboard::F2) { 
+						for (TurtleCommand tc : cmd) {
+							switch (tc) {
+								default:
+									break;
+								case AV: translateY(-delta);  plotTurtle(); break;
+								//...
+							}
+							plotTurtle();
+						}
+						
+					}
+
+					if (event.key.code == sf::Keyboard::F3) {
+						readScript();
+					}
+
 					break;
 				}
 
@@ -280,9 +375,11 @@ int main() {
 			}
 		}
 
-
+		
 		sf::Time dt = deltaClock.restart();
 		ImGui::SFML::Update(window, dt);
+
+		rp.update(dt.asSeconds());
 
 		myFpsCounter.setPosition(8, 8);
 		myFpsCounter.setFillColor(sf::Color::Red);
